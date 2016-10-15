c30c6900ce3c7fd82820aca411acbd956177bcd3
mcapretto <margy.c94@gmail.com>
2016-10-15 16:13:01 -0300

    Exp Mat

--- math/exp.mat.cpp
+++ math/exp.mat.cpp
@@ -2,7 +2,7 @@
 int NN;
 double tmp[SIZE][SIZE];
 void mul(double a[SIZE][SIZE], double b[SIZE][SIZE]){ zero(tmp);
-    forn(i, NN) forn(j, NN) forn(k, NN) res[i][j]+=a[i][k]*b[k][j];
+    forn(i, NN) forn(j, NN) forn(k, NN) tmp[i][j]+=a[i][k]*b[k][j];
     forn(i, NN) forn(j, NN) a[i][j]=res[i][j];
 }
 void powmat(double a[SIZE][SIZE], int n, double res[SIZE][SIZE]){
