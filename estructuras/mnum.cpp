d5484cf00f704ca2da8ae579e4921fb3bebf3084
Fernando Fiori <fefo_f93@hotmail.com>
2017-04-05 23:04:24 -0300

    acomodado para hash

--- estructuras/mnum.cpp
+++ estructuras/mnum.cpp
@@ -1,11 +1,14 @@
+#define MOD 1000000000000000009
+#define PRIME 1009
+
 struct mnum{
-	static const tipo mod=12582917;
+	static const tipo mod=MOD;
 	tipo v;
 	mnum(tipo v=0): v(v%mod) {}
-	mnum operator+(mnum b){return v+b.v;}
-	mnum operator-(mnum b){return v>=b.v ? v-b.v : mod-b.v+v;}
-	mnum operator*(mnum b){return v*b.v;}
+	mnum operator+(mnum b){return (v+b.v)%mod;}
+	mnum operator-(mnum b){return (((v-b.v)%mod)+mod)%mod;}
+	mnum operator*(mnum b){return mul(v,b.v,mod);}
 	mnum operator^(int n){
 		if(!n) return 1;
-		return n%2 ? (*this)^(n/2)*(*this) : (*this)^(n/2);}
+		return n%2 ? ((*this)^(n/2))*(*this) : (*this)^(n/2);}
 };
