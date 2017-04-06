769c58eec347a4b04544ff92ec4acf9ad07ffdcd
Fernando Fiori <fefo_f93@hotmail.com>
2017-04-05 22:49:17 -0300

    Update mnum.cpp

--- estructuras/mnum.cpp
+++ estructuras/mnum.cpp
@@ -3,9 +3,9 @@ struct mnum{
 	tipo v;
 	mnum(tipo v=0): v(v%mod) {}
 	mnum operator+(mnum b){return v+b.v;}
-	mnum operator-(mnum b){return v>=b.v? v-b.v : mod-b.v+v;}
+	mnum operator-(mnum b){return v>=b.v ? v-b.v : mod-b.v+v;}
 	mnum operator*(mnum b){return v*b.v;}
 	mnum operator^(int n){
 		if(!n) return 1;
-		return n%2? (*this)^(n/2)*(this) : (*this)^(n/2);}
+		return n%2 ? (*this)^(n/2)*(*this) : (*this)^(n/2);}
 };
